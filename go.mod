c778a7b065573e8f6f0f6dd61b7dee59f1f49a53
Liang Ding <d@b3log.org>
2019-05-12 19:06:27 +0800

    :art: 签名

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/b3log/starfire-moderator
 
 go 1.12
 
-require github.com/ipfs/go-ipfs-api v0.0.1
+require (
+	github.com/ipfs/go-ipfs-api v0.0.1
+	github.com/libp2p/go-libp2p-crypto v0.0.1
+	github.com/mitchellh/go-homedir v1.1.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"io/ioutil"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"time"
 
 	api "github.com/ipfs/go-ipfs-api"
+crypto "github.com/libp2p/go-libp2p-crypto"
 )
 
 func init() {
@@ -44,6 +50,30 @@ func main() {
 	}
 	fmt.Println("blacklist [" + blacklistId + "]")
 
+	userHome, err := homedir.Dir()
+	if nil != err {
+		panic(err)
+	}
+	configPath := filepath.Join(userHome, ".ipfs-starfire-moderator", "config")
+	data, err := ioutil.ReadFile(configPath)
+	if nil != err {
+		panic(err)
+	}
+	config := map[string]interface{}{}
+	if err := json.Unmarshal(data, &config);nil!=err {
+		panic(err)
+	}
+	identity := config["Identity"].(map[string]interface{})
+	privKey := identity["PrivKey"].(string)
+	data, err = base64.StdEncoding.DecodeString(privKey)
+	if nil != err {
+		panic(err)
+	}
+	key, err := crypto.UnmarshalPrivateKey(data)
+	if nil != err {
+		panic(err)
+	}
+
 	moderateBlacklistCmd := map[string]interface{}{
 		"type": typeBlacklist,
 		"data": blacklistId,
@@ -52,6 +82,9 @@ func main() {
 	if nil != err {
 		panic(err)
 	}
+	signBytes, err := key.Sign(moderateBlacklistCmdBytes)
+	sign := hex.EncodeToString(signBytes)
+	moderateBlacklistCmd["sign"] = sign
 	moderateBlacklistCmdData := string(moderateBlacklistCmdBytes)
 	sh.PubSubPublish(topic, moderateBlacklistCmdData)
 
